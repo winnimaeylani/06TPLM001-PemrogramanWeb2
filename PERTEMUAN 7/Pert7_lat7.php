b39d66a521eb34b9baa9da5b3f7c0955ab46586b
LAPTOP-3K5MTNT5\winni <wini59maylani@gmail.com>
2021-04-16 14:26:42 +0700

    Pertemuan 7

---
+++ PERTEMUAN 7/Pert7_lat7.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+<TITLE> Penggunaan Split </TITLE>
+</HEAD>
+<BODY>
+	<?php
+	$tanggal = "17-05-2010";
+	list($hari, $bulan, $tahun) = preg_split("[-]", $tanggal);
+	echo "Hari = $hari";
+	echo "<br />";
+	echo "Bulan = $bulan";
+	echo "<br />";
+	echo "Tahun = $tahun";
+	?>
+</BODY>
+</HTML>
+
